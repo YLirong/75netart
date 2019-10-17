daa70751c30eba50ee070dc462241075f817f347
YLirong <yanrong.li@sjsu.edu>
2019-10-16 22:44:05 -0700

    p5

--- sketch.js
+++ sketch.js
@@ -1,13 +1,45 @@
-var xoffset = 40;
-var yoffset = 50;
+let photo, mask;
+
+var xoffset = 155;
+var yoffset = 245;
+function preload() {
+  photo = loadImage('emoji.jpg');
+  mask = loadImage('m2.png');
+   // imgMask = loadImage('assets/mask.png');
+
+}
 function setup() {
   createCanvas(400, 400);
+     // background(0);
+  noLoop();
+    imageMode(CORNER);
+
+    image(photo, 0,0);
 }
 
 function draw() {
-  background(220);
+    // background(255);
+   // photo.mask(mask);
+     image(photo, 0, 0);
+  fill(0);
+ellipse(xoffset, yoffset, 30, 30);
 
-   ellipse(xoffset, yoffset, 40, 40);
-  ellipse(xoffset+100, yoffset, 40, 40);
-  arc(xoffset+50, yoffset+50, 100, 50, 0, PI);
+  ellipse(xoffset+120, yoffset+10, 30, 30);
+  stroke(255);
+  strokeWeight(8);
+  noFill();
+  arc(xoffset+50, yoffset+50, 50, 50, 0, PI);
+}
+
+function mousePressed() {
+if(mouseIsPressed){
+ // photo.mask(mask);
+   background(0);
+   image(photo, 0, 0);
+ photo.mask(mask);
+
+
+  }
+
+
 }
